89b1a871f9aedd7797a899b8df4e03479e522f8a
firevaam <firevaam@gmail.com>
2024-09-20 09:13:16 +0200

    ajout de fichier

--- src/Identite.java
+++ src/Identite.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Identite {
+public class Identite {
+
+    private String NIP;
+    private String nom;
+    private String prenom;
+
+    public Identite(String NumIdP, String n, String p){
+        this.NIP = NumIdP;
+        this.nom = n;
+        this.prenom = p;
+    }
+
+    public String getNIP(){
+        return NIP;
+    }
+
+    public String getNom(){
+        return nom;
+    }
+
+    public String getPrenom(){
+        return prenom;
+    }
 }
