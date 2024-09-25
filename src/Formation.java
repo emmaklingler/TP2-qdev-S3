1e6ed57495d2aac150af978f8a26e81e4bc50d43
Klingler Emma <em.klnglr@gmail.com>
2024-09-25 10:47:59 +0200

    conflit resolu

--- src/Formation.java
+++ src/Formation.java
@@ -1,4 +1,9 @@
-<<<<<<< HEAD
+
+
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 
 import java.util.HashMap;
@@ -43,49 +48,4 @@ public class Formation {
         return this.matiere;
     }
 }
-=======
-package tP_formation;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class Formation {
-    private int ID;
-    private HashMap<String, Float> matiere;
-
-    public Formation(int ID) {
-        this.ID = ID;
-        this.matiere = new HashMap<>();
-    }
-
-
-    public void ajouterMatiere(HashMap<String, Float> matierep) {
-        for (String key : matierep.keySet()) {
-            if (!this.matiere.containsKey(key)) {
-                this.matiere.put(key, matierep.get(key));
-            }
-        }
-    }
-
-
-
-    public void supprimerMatiere(HashMap<String, Float>matierep) {
-        for (String key : matierep.keySet()) {
-            if (this.matiere.containsKey(key)) {
-                this.matiere.remove(matierep);
-            }else{ System.out.println("cette matiere n'est pas présente dans la liste");}
-        }
-
-    }
-
-
-    public Float getcoeff(String matiereName) {
-        return this.matiere.get(matiereName);
-    }
-
-    // Méthode pour obtenir toutes les matières
-    public HashMap<String, Float> getMatiere() {
-        return this.matiere;
-    }
-}
->>>>>>> 3d6947ce57a9e87db80cadc4deedfa090c3cd750
